4a3cc0485e0f403f571b722e4cf5e969a9f367a8
Andrew Yip <yipcma@gmail.com>
2015-08-29 18:59:54 +0300

    events&session

--- leaderboard.js
+++ leaderboard.js
@@ -3,7 +3,20 @@ PlayersList = new Mongo.Collection('players');
 if (Meteor.isClient) {
   Template.leaderboard.helpers({
     player: function() {
-      return PlayersList.find()
+      return PlayersList.find();
+    },
+    selectedClass: function() {
+      var selectedPlayer = Session.get("selectedPlayer");
+      var thisRow = this._id;
+      if (selectedPlayer === thisRow) {
+        return "selected"
+      }
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .player': function(event) {
+      Session.set("selectedPlayer", this._id);
     }
   })
 }
